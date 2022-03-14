6b64ce42aca8e7588dd64b7820164acbcfa40193
Michael Maker <maker.mdm@gmail.com>
2022-03-14 18:48:01 -0500

    Create Footer react component

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer>
+        &copy; Michael Maker, 2022
+      </footer>
+    );
+  }
+}
+
+export default Footer;
