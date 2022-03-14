3a2111f44d2527bc05b76ae6db96e0bf73f23194
Michael Maker <maker.mdm@gmail.com>
2022-03-14 18:48:14 -0500

    Create Header react component

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>People of 301d83</h1>
+        <h1>Gallery of Horns</h1>
       </header>
-    )
+    );
   }
 }
 
