6b5f8e1ac4f8242f76cbe3354aa47dfee15412d4
Michael Maker <maker.mdm@gmail.com>
2022-03-14 17:16:16 -0500

    Initial encode

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>People of 301d83</h1>
+      </main>
+    )
+  }
+}
+
+export default Main;
