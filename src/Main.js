17a6823fd5c3907738f9f906f0c1c034531695c7
Michael Maker <maker.mdm@gmail.com>
2022-03-16 22:36:47 -0500

    Placed Horned Beast comonent into Container

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import { Col, Row, Container } from 'react-bootstrap';
 import './Main.css';
 
 class Main extends React.Component {
   render() {
-    // console.log(this.props.data);
-    let hornedBeastArray = [];
-    this.props.data.forEach((hornedBeast,index) => {
-      hornedBeastArray.push(
+    // console.log('main props', this.props);
+    let hornedBeastArray = this.props.data.map((beast, index) => (
+      <Col key={index}>
         <HornedBeast
-          title={hornedBeast.title}
-          image_url={hornedBeast.image_url}
-          key = {index}
-          description = {hornedBeast.description}
+          beast={beast}
+          handleSelectedBeast={this.props.handleSelectedBeast}
         />
-      )
-    })
+      </Col>
+    ));
 
-// console.log(hornedBeastArray);
+    // console.log(hornedBeastArray);
     return (
-      <main>
-        {hornedBeastArray}
-      </main>
+      <Container>
+        <Row xs={1} sm={1} md={2} lg={3} xl={4}>
+          {hornedBeastArray}
+        </Row>
+      </Container>
     )
   }
 }
